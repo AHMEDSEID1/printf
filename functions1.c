e124b10dd8bc0f785e877f22992e367c008cd403
@momodufyakubu <momodufyakubu@gmail.com>
2023-06-27 05:36:41 -0700

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -5,10 +5,10 @@
  * print_unsigned - Prints an unsigned number
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * @flags:  Calculates active flags.
  * @width: get width
  * @precision: Precision specification
- * @size: Size specifier
+ * @size: Size specifier.
  * Return: Number of chars printed.
  */
 int print_unsigned(va_list types, char buffer[],
